a1ff0475b8c968d2eae3a22daaa789a9c387cb7e
KabeloMaps <mpnkab004@myuct.ac.za>
2025-12-28 23:30:22 +0200

    NavBar layout

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,92 @@
-import React from "react";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/react";
+import { XMarkIcon } from "@heroicons/react/24/outline";
+
+const navigation = [
+  { name: "Features", href: "#" },
+  { name: "Company", href: "#" },
+  { name: "Careers", href: "#" },
+  { name: "About", href: "#" },
+];
 
 const NavBar = () => {
   return (
-    <div>
-      <h1>Hello there, Lets play some endgames</h1>
-      <h2>Enter your name: </h2>
-    </div>
+    <Disclosure as="nav" className="relative  bg-white">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center  sm:hidden">
+            {/* Mobile menu button*/}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-black hover:bg-white/5 hover:text-black focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+              <span className=" absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <img
+                src="/images/icon-menu.svg"
+                alt="menu-icon"
+                className="w-auto h-auto pt-1"
+              />
+              <XMarkIcon
+                aria-hidden="true"
+                className="hidden size-6 group-data-open:block"
+              />
+            </DisclosureButton>
+          </div>
+          <div className="flex  flex-1 items-center justify-start sm:items-stretch sm:justify-start">
+            <div className="flex shrink-0 ">
+              <img
+                alt="sneaker-logo"
+                src="/images/logo.svg"
+                className="h-auto w-auto pl-10 "
+              />
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4 ">
+                {navigation.map((item) => (
+                  <a key={item.name} href={item.href}>
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="nav-end  absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <button
+              type="button"
+              className="relative rounded-full p-1 text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
+            >
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">View notifications</span>
+              <button>Login</button>{" "}
+            </button>
+
+            {/* Profile dropdown */}
+            <Menu as="div" className="relative ml-3">
+              <MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                <span className="absolute -inset-1.5" />
+                <span className="sr-only">Open user menu</span>
+                <button>Register</button>
+              </MenuButton>
+            </Menu>
+          </div>
+        </div>
+      </div>
+
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3">
+          {navigation.map((item) => (
+            <DisclosureButton key={item.name} as="a" href={item.href}>
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
   );
 };
 
